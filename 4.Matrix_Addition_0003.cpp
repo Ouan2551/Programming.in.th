ae36003a8dc0df37c18ace52a050edbd10ab745c
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-18 16:23:20 +0700

    -

---
+++ 4.Matrix_Addition_0003.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int m, n; cin >> m >> n; int a[m][n], b[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
+        
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> b[i][j];
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            cout << a[i][j] + b[i][j] << ' '; count++;
+            if (count == 3)
+            {
+                cout << '\n';
+            }
+        }
+    }
+    return 0;
+}
